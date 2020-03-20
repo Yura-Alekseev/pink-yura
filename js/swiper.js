ec2b2feda5300edf9770710d6eaa8859f374b385
Yura Alekseev <yuraalekseev00@gmail.com>
2020-03-20 15:38:51 +0200

    added offers, testimonials

--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,19 @@
 var swiper = new Swiper('.swiper-container', {
     loop: true,
-    navigation: {
-        nextEl: '.arrow-next',
-        prevEl: '.arrow-prev',
-    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets'
+            }
+        },
+        1024 : {
+            navigation: {
+                nextEl: '.arrow-next',
+                prevEl: '.arrow-prev',
+            },
+            simulateTouch: false
+        }
+    }
 });
